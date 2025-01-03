770e44f2aae539a9e0ef03ccbb336d8639cad465
Adriano Enache <adriano.enache@publicisgroupe.com>
2025-01-03 18:18:21 -0300

    Update npm package liquidjs 10.8.2 to 10.20.0

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const loaderUtils = require("loader-utils");
+const loaderUtils = require('loader-utils');
 const { Liquid } = require('liquidjs');
-const path = require('path');
 
 module.exports = function(source, map) {
+
   const options = loaderUtils.getOptions(this) || {};
   const data = options.data || {};
+
   const liquidOptions = Object.assign({}, options);
   delete liquidOptions.data;
   liquidOptions.root = liquidOptions.root || this.context;
+
   const engine = new Liquid(liquidOptions);
+
   this.callback(null, engine.parseAndRenderSync(source, data), map);
+
 };
